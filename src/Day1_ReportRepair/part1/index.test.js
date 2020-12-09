fb61a31ef2b629725df5c6a33ad32f1bc9eec445
lp68ba <jfcorugedo@gmail.com>
2020-12-09 18:50:09 +0100

    Day 1: Report Repair

---
+++ src/Day1_ReportRepair/part1/index.test.js
@@ -0,0 +1,11 @@
+const repair = require('./index');
+const {sample, answer: sampleAnswer} = require('./sample');
+const {dataset, answerPart1} = require('../input');
+
+test('SAMPLE: given the sample dataset the function works with it', () => {
+    expect(repair(sample)).toBe(sampleAnswer);
+});
+
+test('REAL: given the real expense report the function can find the two numbers', () => {
+    expect(repair(dataset)).toBe(answerPart1);
+});
---
+++ src/Day1_ReportRepair/part2/index.test.js
@@ -0,0 +1,11 @@
+const repairV2 = require('./index');
+const {sample, answer} = require('./sample');
+const {dataset, answerPart2} = require('../input');
+
+test('SAMPLE: given the sample dataset the function works with it', () => {
+    expect(repairV2(sample)).toBe(answer);
+});
+
+test('REAL: given the real expense report the function can find the three numbers', () => {
+    expect(repairV2(dataset)).toBe(answerPart2);
+});
