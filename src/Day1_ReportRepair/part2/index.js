429372199d83c7c178c710c084cb4d7bb036211b
lp68ba <jfcorugedo@gmail.com>
2020-12-09 19:34:13 +0100

    Day 1: Report Repair - Part 2 solved

--- src/Day1_ReportRepair/part2/index.js
+++ src/Day1_ReportRepair/part2/index.js
@@ -9,8 +9,34 @@
 
  In your expense report, what is the product of the three entries that sum to 2020?
  */
+
+const repair = require('../part1/index');
+
 function repairV2(report) {
-    //TODO
+
+    const target = 2020;
+
+    const result = report
+        .split('\n')
+        .filter(n => n !== '')
+        .map(n => Number(n))
+        .reduce( (resultFound, first, index, collection) => {
+            if(resultFound) return resultFound;
+
+            const collectionWithoutCurrent = [...collection.slice(0, index), ...collection.slice(index+1)];
+
+            return collectionWithoutCurrent.reduce( (subResultFound, second, subIndex, partialCollection) => {
+                if(subResultFound) return subResultFound;
+
+                const third = partialCollection.slice(subIndex + 1).find( next => (first + second + next) === target  );
+                if(third) {
+                    return {first, second, third};
+                }
+            }, undefined );
+        }, undefined );
+
+    console.log('result', result);
+    return result.first * result.second * result.third;
 }
 
 module.exports = repairV2;
--- src/Day1_ReportRepair/part2/index.test.js
+++ src/Day1_ReportRepair/part2/index.test.js
@@ -2,10 +2,10 @@ const repairV2 = require('./index');
 const {sample, answer} = require('./sample');
 const {dataset, answerPart2} = require('../input');
 
-test('SAMPLE: given the sample dataset the function works with it', () => {
+test('SAMPLE(part2): given the sample dataset the function works with it', () => {
     expect(repairV2(sample)).toBe(answer);
 });
 
-test('REAL: given the real expense report the function can find the three numbers', () => {
+test('REAL(part2): given the real expense report the function can find the three numbers', () => {
     expect(repairV2(dataset)).toBe(answerPart2);
 });
